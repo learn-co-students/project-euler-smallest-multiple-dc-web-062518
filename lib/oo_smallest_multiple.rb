95982d010625e48c931be43c2787615425a7b647
github username <github email address>
2018-09-25 09:54:24 -0400

    Done.

--- lib/oo_smallest_multiple.rb
+++ lib/oo_smallest_multiple.rb
@@ -1,1 +1,25 @@
 # Implement your object-oriented solution here!
+class SmallestMultiple
+
+def initialize(num)
+  @num = num
+end
+
+def lcm
+  arr = (2..@num).to_a
+  mult = 0
+  counter = @num * (@num - 1)
+  while mult == 0
+    arr.each do |n|
+      if counter % n != 0
+        counter += @num
+        break
+      elsif n == @num && counter % n == 0
+        mult = counter
+      end
+    end
+  end
+  return mult
+end
+
+end
