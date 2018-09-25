3e6aeeec4dcb357dc9a5d2afc7d0fffdf5994d8a
github username <github email address>
2018-09-25 10:29:56 -0400

    Done.

--- lib/smallest_multiple.rb
+++ lib/smallest_multiple.rb
@@ -1,1 +1,19 @@
 # Implement your procedural solution here!
+def smallest_multiple(max)
+  arr = (1..max).to_a
+  counter = 0
+
+  check = false
+  while check == false
+    counter+=max
+    arr.each do |n|
+      if counter % n != 0
+        check = false
+        break
+      else
+        check = true
+      end
+    end
+  end
+  counter
+end
